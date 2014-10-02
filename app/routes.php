b021763e2c5a53aded5f4a5f07254b8f805f23f7
Jonas Duri <jd@oygo.com>
2014-10-02 17:32:56 +0200

    first commit

---
+++ app/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+Route::get('user/{id}', 'UsersController@index');
+Route::get('auth', 'Tappleby\AuthToken\AuthTokenController@index');
+Route::post('auth', 'Tappleby\AuthToken\AuthTokenController@store');
+Route::delete('auth', 'Tappleby\AuthToken\AuthTokenController@destroy');
---
+++ readme.md
@@ -0,0 +1,2 @@
+## A Simple Laravel api Server
+
